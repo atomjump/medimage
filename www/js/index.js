44730da41d9616d890ec6634debcb423dfbd9572
AtomJump <peter@atomjump.com>
2019-10-02 15:11:03 +1300

    first test at a wound drawing link

--- www/js/index.js
+++ www/js/index.js
@@ -801,7 +801,9 @@ var app = {
 		//If there is a link to a MedImage Server Wound Mapp add-on saved (TODO), show 
 		//a button to 
 		//TODO: turn into generic URL and folder version of imageURI
-		glbThis.cancelNotify("<ons-icon style=\"vertical-align: middle; color:#f7afbb;\" size=\"30px\" icon=\"fa-close\" href=\"#javascript\" onclick=\"window.open(\"http:\/\/104.131.151.99:5567\/addon\/show-analysis?photo=" + imageURI + "\&style=mob\", \"_system\");\"></ons-icon><br/>Measure Wound");  
+		var url = "http://104.131.151.99:5567/addon/show-analysis?photo=" + imageURI + "&style=mob";
+		
+		glbThis.cancelNotify("<ons-icon style=\"vertical-align: middle; color:#f7afbb;\" size=\"30px\" icon=\"fa-close\" href=\"#javascript\" onclick=\"alert('Trying to open:" + url + "'; window.open(\"encodeURI('" + url + "')\", \"_system\");\"></ons-icon><br/>Measure Wound");  
 		//   
 	},
 						
